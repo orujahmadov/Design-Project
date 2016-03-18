8c047293398fa5b87bf630b0b6c1ba8aa4e3bc60
orujahmadov <oruc888@yahoo.com>
2016-03-18 02:59:41 -0400

    chart added

---
+++ src/jfreeChart/Chart.java
@@ -0,0 +1,146 @@
+package jfreeChart;
+
+/*
+ * ==================
+ *
+ * Copyright (c) 2005-2014, Object Refinery Limited.
+ * All rights reserved.
+ *
+ * http://www.jfree.org/jfreechart/index.html
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   - Neither the name of the Object Refinery Limited nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL OBJECT REFINERY LIMITED BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Original Author:  David Gilbert (for Object Refinery Limited);
+ * Contributor(s):   -;
+ *
+ * Changes
+ * -------
+ * 09-Mar-2005 : Version 1 (DG);
+ * 11-Mar-2014 : Use new ChartFactory method (DG);
+ * 25-Jun-2014 : Update to use real data (DG);
+ *
+ */
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.StandardChartTheme;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYBarRenderer;
+import org.jfree.chart.title.TextTitle;
+import org.jfree.data.xy.IntervalXYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+import java.awt.*;
+import java.util.Random;
+
+/**
+ * A simple demonstration application to create a bar chart.
+ */
+class Chart extends ApplicationFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    static {
+        // set a theme using the new shadow generator feature available in
+        // 1.0.14 - for backwards compatibility it is not enabled by default
+        ChartFactory.setChartTheme(new StandardChartTheme("JFree/Shadow",
+                true));
+    }
+
+    /**
+     * Creates a new demo instance.
+     *
+     * @param title  the frame title.
+     */
+    public Chart(String title) {
+        super(title);
+        IntervalXYDataset dataset = createDataset();
+        JFreeChart chart = createHistogram(dataset);
+        ChartPanel chartPanel = new ChartPanel(chart, false);
+        chartPanel.setBackground(null);
+        chartPanel.setFillZoomRectangle(true);
+        chartPanel.setMouseWheelEnabled(true);
+        chartPanel.setDismissDelay(Integer.MAX_VALUE);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+        setContentPane(chartPanel);
+    }
+
+    /**
+     * Returns a sample dataset.
+     *
+     * @return The dataset.
+     */
+    private static IntervalXYDataset createDataset() {
+        final XYSeries series = new XYSeries("Cars");
+        for(int i=0; i <= 100; i++) {
+            Random random = new Random();
+            series.add(i,random.nextInt(1000));
+        }
+        final XYSeriesCollection dataset = new XYSeriesCollection(series);
+        return dataset;
+    }
+
+    /**
+     * Creates a sample chart.
+     *
+     * @param dataset  the dataset.
+     *
+     * @return The chart.
+     */
+    private static JFreeChart createHistogram(IntervalXYDataset dataset) {
+        JFreeChart chart = ChartFactory.createHistogram(
+                "Demand Response on Electric Vehicles", null /* x-axis label*/,
+                "Disconnected cars" /* y-axis label */, dataset, PlotOrientation.VERTICAL, true, true, true);
+        chart.addSubtitle(new TextTitle("This graph displays number of "
+                + "disconnected cars at different times of request"));
+        chart.setBackgroundPaint(null);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(null);
+
+
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+        XYBarRenderer renderer = (XYBarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        chart.getLegend().setFrame(BlockBorder.NONE);
+        return chart;
+    }
+
+    /**
+     * Starting point for the demonstration application.
+     */
+    public static void initialize() {
+        Chart demo = new Chart("JFreeChart: Histogram");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+    }
+
+}
